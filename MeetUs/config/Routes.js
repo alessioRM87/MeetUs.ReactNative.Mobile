27ef43e8c240fe67f704ecec4d62132874c072c1
alessioRM87 <alessio.iannella@gmail.com>
2018-03-19 13:42:47 -0400

    // ROUTES
    Set routes to login, registration and home

---
+++ MeetUs/config/Routes.js
@@ -0,0 +1,11 @@
+import Home from '../components/home';
+import Login from '../components/login';
+import Registration from '../components/registration';
+
+const Routes = {
+    Login : { screen: Login },
+    Registration: { screen: Registration },
+    Home : { screen: Home },
+};
+
+export default Routes;
