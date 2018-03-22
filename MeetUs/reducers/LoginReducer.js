63658fb7a734619dc6769c9a07f47573a57fa0e6
alessioRM87 <alessio.iannella@gmail.com>
2018-03-22 14:56:03 -0400

    // LOGIN REDUCER
    Reducer for login receiving state and action

--- MeetUs/reducers/LoginReducer.js
+++ MeetUs/reducers/LoginReducer.js
@@ -1,6 +1,8 @@
 const initialState = {
-    username : "",
+    email : "",
+    emailError: "",
     password : "",
+    passwordError: "",
     loading: false,
     success : false,
     error : null
@@ -8,6 +10,47 @@ const initialState = {
 
 export default function loginReducer(state = initialState, action = {}){
     switch (action.type){
+        case "LOGIN_SET_EMAIL":
+            return {
+                ...state,
+                email: action.email
+            };
+        case "LOGIN_SET_EMAIL_ERROR":
+            return {
+                ...state,
+                emailError: action.emailError
+            };
+        case "LOGIN_SET_PASSWORD":
+            return {
+                ...state,
+                password: action.password
+            };
+        case "LOGIN_SET_PASSWORD_ERROR":
+            return {
+                ...state,
+                passwordError: action.passwordError
+            };
+        case "LOGIN_LOADING":
+            return {
+                ...state,
+               loading: true,
+               success: false,
+               error: ""
+            };
+            case "LOGIN_SUCCESS":
+            return {
+                ...state,
+                login: false,
+                success: true,
+                error: ""
+            };
+            case "LOGIN_ERROR":
+            return {
+                ...state,
+                loading: false,
+                success: false,
+                error: action.error
+            };
         default: 
             return state;
     }
