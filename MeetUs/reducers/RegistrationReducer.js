1aa9e2a0503623d102257805e691683e9c78618e
alessioRM87 <alessio.iannella@gmail.com>
2018-03-24 14:21:08 -0400

    // REGISTRATION REDUCER
    Reducer for registation with state and actions

--- MeetUs/reducers/RegistrationReducer.js
+++ MeetUs/reducers/RegistrationReducer.js
@@ -1,7 +1,14 @@
 const initialState = {
-    username : "",
+    email : "",
+    emailError: "",
+    firstName: "",
+    firstNameError: "",
+    lastName: "",
+    lastNameError: "",
     password : "",
+    passwordError: "",
     confirmPassword : "",
+    confirmPasswordError: "",
     loading : false,
     success : false,
     error : null
@@ -9,6 +16,87 @@ const initialState = {
 
 export default function registrationReducer(state = initialState, action = {}){
     switch (action.type){
+        case "REGISTRATION_SET_EMAIL":
+            return {
+                ...state,
+                email: action.email
+            };
+        case "REGISTRATION_SET_EMAIL_ERROR":
+            return {
+                ...state,
+                emailError: action.emailError
+            };
+        case "REGISTRATION_SET_FIRST_NAME":
+            return {
+                ...state,
+                firstName: action.firstName
+            };
+        case "REGISTRATION_SET_FIRST_NAME_ERROR":
+            return {
+                ...state,
+                firstNameError: action.firstNameError
+            };
+        case "REGISTRATION_SET_LAST_NAME":
+            return {
+                ...state,
+                lastName: action.lastName
+            };
+        case "REGISTRATION_SET_LAST_NAME_ERROR":
+            return {
+                ...state,
+                lastNameError: action.lastNameError
+            };
+        case "REGISTRATION_SET_PASSWORD":
+            return {
+                ...state,
+                password: action.password
+            };
+        case "REGISTRATION_SET_PASSWORD_ERROR":
+            return {
+                ...state,
+                passwordError: action.passwordError
+            };
+        case "REGISTRATION_SET_CONFIRM_PASSWORD":
+            return {
+                ...state,
+                confirmPassword: action.confirmPassword
+            };
+        case "REGISTRATION_SET_CONFIRM_PASSWORD_ERROR":
+            return {
+                ...state,
+                confirmPasswordError: action.confirmPasswordError
+            };
+        case "REGISTRATION_SET_EMAIL":
+            return {
+                ...state,
+                email: action.email
+            };
+        case "REGISTRATION_SET_EMAIL":
+            return {
+                ...state,
+                email: action.email
+            };
+        case "REGISTRATION_LOADING":
+            return {
+                ...state,
+                loading: true,
+                success: false,
+                error: ""
+            };
+        case "REGISTRATION_SUCCESS":
+            return {
+                ...state,
+                loading: false,
+                success: true,
+                error: ""
+            };
+        case "REGISTRATION_ERROR":
+            return {
+                ...state,
+                loading: false,
+                success: false,
+                error: action.error
+            };
         default : 
             return state;
     }
