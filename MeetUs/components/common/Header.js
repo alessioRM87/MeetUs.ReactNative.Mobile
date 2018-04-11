8dd8ccb2dd62f64975c8092d6490cf17564c3033
Alessio Iannella <alessio.iannella@gmail..com>
2018-04-11 14:48:26 -0400

    // HEADER
    Created header component

---
+++ MeetUs/components/common/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+class Header extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.headerText} allowFontScaling={false}>{this.props.headerText}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 60,
+        backgroundColor: "#009800",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+        alignSelf: 'center',
+        fontSize: 32,
+        fontWeight: 'bold',
+        fontFamily: 'Helvetica'
+    }
+});
+
+function mapStateToProps (state) {
+    return {
+    };
+}
+function mapDispatchToProps(dispatch){
+    return {
+    };
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Header)
