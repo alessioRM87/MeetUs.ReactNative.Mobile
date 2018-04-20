1624f8dbc3cb79907e29ea1f4f6df423d7db4679
alessioRM87 <alessio.iannella@gmail.com>
2018-04-20 19:01:06 -0400

    // SET NEW SERVER URL

--- MeetUs/config/Config.js
+++ MeetUs/config/Config.js
@@ -1,2 +1,1 @@
-// export const serverURL = "https://meetusappreactnative.herokuapp.com/api";
-export const serverURL = "https://meet-us-server1.herokuapp.com/api";
+export const serverURL = "https://meetusappreactnative.herokuapp.com/api";
